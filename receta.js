b9f42b35aee018f4ceffd95b369d3c5833ff8eca
Sofia Restrepo <sofiarestrepol@gmail.com>
2023-09-21 20:10:38 -0500

    ensayo 1

---
+++ receta.js
@@ -0,0 +1,29 @@
+//GLOBALES
+
+var COSTOS = {
+  BAJO: "barato",
+  MEDIO: "hola"
+};
+// la idea de esto es que en otros casos se pueda cambiar el contenido (value) pero no la key para no tener que cambiar el codigo
+
+
+//MI RECETA
+
+var titulo = "Receta de Galletas de Mantequilla";
+
+
+var ingredientes = ["azucar", "leche","huevos" ];
+
+var pasoPaso = {
+  paso1: "hola",
+  paso2: "hola",
+  paso3: "hola",
+  paso4: "hola",
+  paso5: "hola",
+  paso1: "hola",
+}
+
+
+
+console.log(titulo)
+console.log(ingredientes)
