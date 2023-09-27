057c6b8396bbdf6f4748ced5576862e7d6c2e1a7
Marvin Cely <mardancely@gmail.com>
2023-09-26 20:37:46 -0500

    feat: delete unexcpected files

--- tipos-datos.js
+++
@@ -1,7 +0,0 @@
-var nombre = 'Marvin';
-var numeroFavorito = 7.545465;
-var tieneHambre = true;
-
-console.log(nombre, typeof nombre);
-console.log(numeroFavorito, typeof numeroFavorito);
-console.log(tieneHambre, typeof tieneHambre);
