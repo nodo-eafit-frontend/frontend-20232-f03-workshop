2c5e568398a1e4b06d4194de5236bb28ea1cd1d0
Marvin Cely <mardancely@gmail.com>
2023-10-10 20:25:31 -0500

    javascriopt calculadora

---
+++ dantecely/book-recipe/pages/create-cooking-recipe/create-cooking-recipe.js
@@ -0,0 +1,19 @@
+// const titleRecipe = document.querySelector('#title-recipe').value;
+
+// sumar 2 numeros dinámicos
+
+function sum(param1, param2) {
+  return param1 + param2;
+}
+
+const substract = function (param1, param2) {
+  return param1 - param2;
+};
+
+(param1, param2) => {
+  return param1 * param2;
+};
+
+const result = multiply(10, 10);
+
+console.log(result);
