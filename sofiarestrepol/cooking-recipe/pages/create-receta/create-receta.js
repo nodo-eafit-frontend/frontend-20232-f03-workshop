2d32fbc0f692de4706b43d14d069d010afcb14c7
Sofia Restrepo <sofiarestrepol@gmail.com>
2023-10-12 22:11:58 +0200

    feat: add images

--- sofiarestrepol/cooking-recipe/pages/create-receta/create-receta.js
+++ sofiarestrepol/cooking-recipe/pages/create-receta/create-receta.js
@@ -1,1 +1,20 @@
-console.log("hola");
+// const title = document.querySelector('h1');
+// console.log(title);
+
+
+function sumar(param1, param2) {
+  return param1 + param2
+};
+console.log(sumar(20,3)); 
+
+const restar = function(param1, param2) {
+  return param1 - param2
+}
+var resultado = restar(20,2);
+console.log(resultado);
+
+const multiply = (param1,param2) => {
+  return param1 * param1
+};
+var resultado = multiply(20,2);
+console.log(resultado);
