424d0ace71d48e972f65ec18eda457ba4da4acca
Marvin Cely <mardancely@gmail.com>
2023-10-18 19:23:27 -0500

    feat: add validation example

--- dantecely/book-recipe/pages/create-cooking-recipe/create-cooking-recipe.js
+++ dantecely/book-recipe/pages/create-cooking-recipe/create-cooking-recipe.js
@@ -18,32 +18,32 @@ function onSubmit(event) {
   event.preventDefault();
   const formEl = event.target;
 
-  recipe.title = formEl.querySelector('#title-recipe').value;
+  recipe.title = formEl.querySelector("#title-recipe").value;
   recipe.steps = formEl.querySelector('textarea[name="steps"]').value;
-  recipe.time = Number(formEl.querySelector('#time-for-cooking').value);
-  recipe.cost = getRadioResult(formEl, 'cost');
-  recipe.level = getRadioResult(formEl, 'level');
-  recipe.peopleAmount.min = +formEl.querySelector('#min-people').value;
-  recipe.peopleAmount.max = +formEl.querySelector('#max-people').value;
+  recipe.time = Number(formEl.querySelector("#time-for-cooking").value);
+  recipe.cost = getRadioResult(formEl, "cost");
+  recipe.level = getRadioResult(formEl, "level");
+  recipe.peopleAmount.min = +formEl.querySelector("#min-people").value;
+  recipe.peopleAmount.max = +formEl.querySelector("#max-people").value;
 
   // Validaciones
   let validation = {
     isValid: true,
-    message: '',
+    message: "",
   };
 
   if (recipe.title.length > 20) {
     validation.isValid = false;
-    validation.message = 'El título supero el máximo 20 caracteres';
+    validation.message = "El título supero el máximo 20 caracteres";
   }
 
   if (recipe.peopleAmount.min >= recipe.peopleAmount.max) {
     validation.isValid = false;
-    validation.message = 'Cantidad de personas inválidas';
+    validation.message = "Cantidad de personas inválidas";
   }
 
   if (!validation.isValid) {
-    alert('Formulario inválido: ' + validation.message);
+    alert("Formulario inválido: " + validation.message);
   } else {
     console.log(recipe);
   }
