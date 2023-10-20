d5e10a5037bac03f82d38442ff00894a9acca8fc
Sofia Restrepo <sofiarestrepol@gmail.com>
2023-10-20 19:22:42 +0200

    feat: final corrections

--- sofiarestrepol/cooking-recipe/pages/create-receta/create-receta.js
+++ sofiarestrepol/cooking-recipe/pages/create-receta/create-receta.js
@@ -1,20 +1,71 @@
-// const title = document.querySelector('h1');
-// console.log(title);
 
-
-function sumar(param1, param2) {
-  return param1 + param2
+//obtener dificultad
+function obtenerResultado(element, name) {
+  return element.querySelector(`input[name="${name}"]:checked`)?.value;
 };
-console.log(sumar(20,3)); 
 
-const restar = function(param1, param2) {
-  return param1 - param2
-}
-var resultado = restar(20,2);
-console.log(resultado);
 
-const multiply = (param1,param2) => {
-  return param1 * param1
+//primero se define el objeto vacio
+const recipe = { //objeto con la info de la receta
+  title: undefined, //no hay valor por ahora ent se pone undefined
+  portions: undefined,
+  time: undefined,
+  level: undefined,
+  pasos: undefined,
 };
-var resultado = multiply(20,2);
-console.log(resultado);
+
+//despues se llena la info
+function onSubmit(event) { //when click on submit.. do event
+  event.preventDefault(); //evita que se haga la accion predeterminada
+  const formEl = event.target; 
+
+  recipe.title = formEl.querySelector("#title").value;
+  recipe.portions = Number(formEl.querySelector("#portions").value);
+  recipe.time = Number(formEl.querySelector("#prep-time").value);
+  recipe.level = obtenerResultado(formEl, 'level');
+  recipe.steps = formEl.querySelector("textarea[name='steps']").value;
+  
+
+  //Validaciones
+  // let validation = {
+  //   isValid: true,
+  //   message: "",
+  // };
+
+  let isValid = true;
+  let message = "";
+
+  if (recipe.title.length > 20) {
+    isValid = false;
+    message = "El titulo excede 20 caracteres";
+  } else if (recipe.portions < 1) {
+    isValid = false;
+    message = "El numero de porciones debe ser superior o igual a 1";
+  } else if (recipe.time > 180) {
+    isValid = false;
+    message = "El tiempo de preparacion debe ser inferior a 3 horas";
+  } 
+
+  if (!isValid) {
+    alert(`Formulario Invalido. ${message}`);
+  } else {
+    console.log(recipe);
+  }
+  //duda: de que sirve poner el mensaje en las alertas si en el html ya definimos los limites
+
+} // cierra onSubmit
+
+
+
+// switch () {
+//   case value:
+    
+//     break;
+
+//   default:
+//     break;
+// }
+
+//DUDA:
+//cuando se definen variables ( let var = x) y cuando no (var = x)
+
