60b795fa27f2b15650b096fbc0865c22d9d58ab8
Marvin Cely <mardancely@gmail.com>
2023-11-02 18:46:29 -0500

    feat: check validator with append html

--- smcastril1/2-cooking-recipe-forms-a11y/create-recipe/create_recipe.js
+++ smcastril1/2-cooking-recipe-forms-a11y/create-recipe/create_recipe.js
@@ -1,10 +1,8 @@
 //alert("Let's create our proper cooking recipe!!! ");
 
-
 // // TOMAR ELEMENTOS DE EL HTML -----------------------------------------------------------
 // const element =  document.querySelectorAll("label");
 // console.log(element[0].innerText)
-
 
 // // RECUPERAR INPUTS DE EL FORM ----------------------------------------------------------
 // const title = document.querySelector('#title-recipe').value;
@@ -14,28 +12,25 @@
 // const elemento = document.querySelector("fieldset")
 // console.log(elemento.children[1]);
 
-
 // class Recipe{
 //     constructor(){
 
 //     }
 // }
 
-
-
 // class Recipe {
-    // constructor() {
-    //     this.title = undefined;
-    //     this.peopleAmount = {
-    //         max: undefined,
-    //         min: undefined,
-    //     };
-    //     this.time = undefined;
-    //     this.cost = undefined;
-    //     this.type = undefined;
-    //     this.level = undefined;
-    //     this.steps = undefined;
-    // }
+// constructor() {
+//     this.title = undefined;
+//     this.peopleAmount = {
+//         max: undefined,
+//         min: undefined,
+//     };
+//     this.time = undefined;
+//     this.cost = undefined;
+//     this.type = undefined;
+//     this.level = undefined;
+//     this.steps = undefined;
+// }
 
 //     set_radio_button_attribute(element_name){
 
@@ -49,48 +44,49 @@
 
 // }
 
-    // const my_recipe = new Recipe();
-    // my_recipe.set_attribute("title", "#title-recipe");
-    // my_recipe.set_attribute("time", "#time-for-cooking");
-    // my_recipe.set_radio_button_attribute("cost");
-    // my_recipe.set_radio_button_attribute("type");
-    // my_recipe.set_radio_button_attribute("level");
-    // my_recipe.set_attribute("steps", "#steps");
+// const my_recipe = new Recipe();
+// my_recipe.set_attribute("title", "#title-recipe");
+// my_recipe.set_attribute("time", "#time-for-cooking");
+// my_recipe.set_radio_button_attribute("cost");
+// my_recipe.set_radio_button_attribute("type");
+// my_recipe.set_radio_button_attribute("level");
+// my_recipe.set_attribute("steps", "#steps");
 
-    // console.log(my_recipe);
-
+// console.log(my_recipe);
 
 // ------------------------------------------------------------------------------
 
 let data;
 
+function onSubmit(event) {
+  let isValid = true;
+  let msg = '';
 
-function onSubmit(event){
-    let isValid = true;
-    let msg;
+  event.preventDefault(); //No redirecciona
+  data = Object.fromEntries(new FormData(event.target));
+  console.log(data);
 
-    event.preventDefault(); //No redirecciona
-    data = Object.fromEntries(new FormData(event.target));
-    console.log(data);
+  //VALIDACIONES
+  if (data.title.length > 20) {
+    isValid = false;
+    msg += 'ERROR: Title too long.';
+  }
 
+  if (data.min_people >= data.max_people) {
+    console.log('ENTRÉ');
+    isValid = false;
+    msg += 'ERROR: The minimum of people has to be shorter than the maximum.';
 
-    //VALIDACIONES
-    if (data.title.length > 20){
-        isValid = false;
-        msg += "ERROR: Title too long."
-    }
-    
+    const fieldsetEl = document.querySelector('fieldset.people-amount');
+    const messageErrorEl = document.createElement('span');
+    messageErrorEl.className = 'error-message';
+    messageErrorEl.textContent = msg;
 
-    if (data.min_people >= data.max_people){
-        console.log("ENTRÉ")
-        isValid = false;
-        msg += "ERROR: The minimum of people has to be shorter than the maximum."
-    }
+    fieldsetEl.appendChild(messageErrorEl);
+  }
 
-
-
-    if(!isValid){
-        alert(msg)
-    }
-
+  if (!isValid) {
+    alert(msg);
+    s;
+  }
 }
