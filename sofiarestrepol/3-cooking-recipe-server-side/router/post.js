f8516ff95f4a51ad9f601463d89f32c630b159b9
Sofia Restrepo <sofiarestrepol@gmail.com>
2023-11-02 18:17:28 +0100

    feat: post.js updated

--- sofiarestrepol/3-cooking-recipe-server-side/router/post.js
+++ sofiarestrepol/3-cooking-recipe-server-side/router/post.js
@@ -11,36 +11,29 @@ const publishRecipe = (req, res) => {
 
   const { body: newRecipe } = req; // aka const newRecipe =  body 
 
-  fs.readFile("./database/recipes.json", (err, data) => {
+  fs.readFile("./database/recipes.json", (err, data) => { //leer recipes.json
     if (err) {
       const msgError = "Error reading file: " + err; //crea mensaje de error y envia el error encontrado
       console.error(msgError); 
       res.status(400).send(msgError); //duda: por que se imprime el codigo en console.log y luego se envia en res.send ? 
     } else {
       const recipes = JSON.parse(data);
-      newRecipe.id = uuidv4();
+      newRecipe.id = uuidv4(); //newRecipe es una cte que no cambia (const) pero sus hijos (id) si pueden cambiar - por eso se puede sobreescribir
       recipes.push(newRecipe);
-      // res.status(201).send(newRecipe);
+      res.status(201).send(newRecipe);
 
-      // fs.writeFile("./database/recipes.json", JSON.stringify(recipes), (err) => {
-      //   if (err) {
-      //     const msgError = "Error writing file: " + err;
-      //     console.error(msgError);
-      //     res.status(400).send(msgError);
-      //   }
-      //   res.status(201).send(newRecipe);
-      // });
+      fs.writeFile("./database/recipes.json", JSON.stringify(recipes), (err) => { //sobreescribir recipes.json
+        if (err) {
+          const msgError = "Error writing file: " + err;
+          console.error(msgError);
+          res.status(400).send(msgError);
+        }
+        res.status(201).send(newRecipe);
+      });
     }
   })
 }
 
-
-
-//   res.status(200).send({
-//     message: 'recipe published'
-//   })
-//   console.log("published recipe");
-// }
 
 module.exports = { publishRecipe }
 
