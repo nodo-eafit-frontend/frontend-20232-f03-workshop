0a62159ed7a2e4ff1e69288c2f693093e85afe7b
SaraCastril1 <smcastril1@eafit.edu.co>
2023-11-06 13:26:16 -0500

    feat: simple server

--- smcastril1/3-cooking-recipe-server-side/index.js
+++ smcastril1/3-cooking-recipe-server-side/index.js
@@ -1,1 +1,22 @@
-console.log("HOLA SARITA")
+require("dotenv").config();
+const express =  require("express")
+
+// Create my app
+const app = express();
+const { PORT } = process.env || 8080; 
+
+
+recipe = {
+    title: "Kiwi Jam"
+}
+
+//midleware
+const callback = (req, res) => {
+    res.send(recipe);
+}
+
+app.get("/", callback)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
