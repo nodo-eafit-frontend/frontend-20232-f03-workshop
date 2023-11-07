fa5b247deb2efdcfb05d0d21c08f8d2df5e1fb0a
diego jaramillo <diego97jaramillo@gmail.com>
2023-11-07 13:59:17 -0500

    feat: se añadieron todos los cambios que se hicierno cuando se le hizo merge a la branch

--- diego97jaramillo/server-recipe/index.js
+++ diego97jaramillo/server-recipe/index.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-<<<<<<< HEAD
 const express = require("express");
 const router = require("./router");
 const cors = require("cors");
@@ -16,20 +15,4 @@ app.use(router);
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
-=======
-const express = require('express');
-const router = require('./router');
-const cors = require('cors');
-
-const app = express();
-const { PORT } = process.env;
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(router);
-
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
->>>>>>> 3eaf0f01df1e2cf1ddec2cd949abb1d4971a8679
 });
