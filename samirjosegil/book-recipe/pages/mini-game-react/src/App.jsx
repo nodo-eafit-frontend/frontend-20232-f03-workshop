0221724f9924dd9a005322d8a7613dd49f8c5b53
samirjosegil <samirosorio21@gmail.com>
2023-11-12 13:22:46 -0500

    Feat: bugs fix

--- samirjosegil/book-recipe/pages/mini-game-react/src/App.jsx
+++ samirjosegil/book-recipe/pages/mini-game-react/src/App.jsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import Card from './components/Card/Card';
+import Board from './components/Board/Board';
+import Scoreboard from './components/Scoreboard/Scoreboard';
 import './styles/App.scss';
 
-// Definir constantes directamente aquí
-const CARD_COLUMNS = 6;
-const CARD_ROWS = 3;
-const CARD_PAIRS = (CARD_COLUMNS * CARD_ROWS) / 2;
-const CARD_IMAGE_PREFIX = 'imagen';
-const CARD_IMAGE_EXTENSION = '.jpg';
-
 const App = () => {
+  const [cards, setCards] = useState([]);
+  const [flippedCards, setFlippedCards] = useState(new Set());
+  const [gameStarted, setGameStarted] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const CARD_COLUMNS = 6;
+  const CARD_ROWS = 3;
+  const CARD_PAIRS = (CARD_COLUMNS * CARD_ROWS) / 2;
+  const CARD_IMAGE_PREFIX = 'imagen';
+  const CARD_IMAGE_EXTENSION = '.jpg';
+
   const generateCards = () => {
     const uniquePairs = [...Array(CARD_PAIRS).keys()];
     const allPairs = [...uniquePairs, ...uniquePairs];
@@ -23,11 +28,6 @@ const App = () => {
     }));
   };
 
-  const [cards, setCards] = useState([]);
-  const [flippedCards, setFlippedCards] = useState(new Set());
-  const [gameStarted, setGameStarted] = useState(false);
-  const [score, setScore] = useState(0);
-
   const handleCardClick = (id) => {
     if (flippedCards.size < 2 && !flippedCards.has(id)) {
       setCards((prevCards) =>
@@ -39,12 +39,24 @@ const App = () => {
     }
   };
 
+  const handleStartGame = () => {
+    setGameStarted(true);
+    setCards(generateCards());
+    setScore(0);
+  };
+
+  const handleRestartGame = () => {
+    setCards(generateCards());
+    setScore(0);
+    setFlippedCards(new Set());
+  };
+
   useEffect(() => {
     if (flippedCards.size === 2) {
       const [card1, card2] = [...flippedCards].map(
         (flippedId) => cards.find((card) => card.id === flippedId) || {}
       );
-  
+
       if (card1.image === card2.image) {
         setCards((prevCards) =>
           prevCards.map((card) =>
@@ -62,48 +74,30 @@ const App = () => {
           setScore((prevScore) => Math.max(prevScore - 50, 0));
         }, 500);
       }
-  
+
       setFlippedCards(new Set());
     }
   }, [flippedCards, cards]);
 
-  const handleStartGame = () => {
-    setGameStarted(true);
-    setCards(generateCards());
-    setScore(0);
-  };
-
-  const handleRestartGame = () => {
-    setCards(generateCards());
-    setScore(0);
-    setFlippedCards(new Set());
-  };
-
   return (
     <div className="App">
-      <header>
-        <h1>Memory Game</h1>
-        {!gameStarted && <button onClick={handleStartGame}>Start Game</button>}
-        {gameStarted && <button onClick={handleRestartGame}>Restart Game</button>}
+      <header className="App-header">
+        <h1 className="App-title">Memory Game</h1>
+        {!gameStarted && (
+          <button className="App-button" onClick={handleStartGame}>
+            Start Game
+          </button>
+        )}
+        {gameStarted && (
+          <button className="App-button" onClick={handleRestartGame}>
+            Restart Game
+          </button>
+        )}
       </header>
       {gameStarted && (
         <main>
-          <div className="game-container">
-            {cards.map((card) => (
-              <Card
-                key={card.id}
-                id={card.id}
-                image={card.image}
-                isFlipped={card.isFlipped}
-                isMatched={card.isMatched}
-                handleClick={() => handleCardClick(card.id)}
-              />
-            ))}
-          </div>
-          <div className="scoreboard">
-            <h2>Scoreboard</h2>
-            <p>Score: {score}</p>
-          </div>
+          <Board cards={cards} handleCardClick={handleCardClick} />
+          <Scoreboard score={score} />
         </main>
       )}
     </div>
---
+++ samirjosegil/book-recipe/pages/mini-game-react/src/components/Board/Board.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from '../Card/Card';
+
+const Board = ({ cards, handleCardClick }) => (
+  <div className="game-container">
+    {cards.map((card) => (
+      <Card
+        key={card.id}
+        id={card.id}
+        image={card.image}
+        isFlipped={card.isFlipped}
+        isMatched={card.isMatched}
+        handleClick={() => handleCardClick(card.id)}
+      />
+    ))}
+  </div>
+);
+
+export default Board;
