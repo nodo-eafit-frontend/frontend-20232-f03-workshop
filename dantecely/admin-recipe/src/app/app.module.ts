f9374c714b88c2bfd0c0d4ff8e8ecf083684175d
Marvin Cely <mardancely@gmail.com>
2023-11-13 21:44:34 -0500

    implement Angular

--- dantecely/admin-recipe/src/app/app.module.ts
+++ dantecely/admin-recipe/src/app/app.module.ts
@@ -4,19 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { MaterialModule } from './material/material.module';
+import { ImageComponent } from './components/image/image.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeEditorComponent } from './components/recipe-editor/recipe-editor.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ImageComponent, RecipeListComponent, RecipeEditorComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
