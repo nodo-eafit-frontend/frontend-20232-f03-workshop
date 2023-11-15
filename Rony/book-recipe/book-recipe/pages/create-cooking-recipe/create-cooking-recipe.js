0180002c78992e8af3cc22bcece7fbab686654bb
Ronycore <127463614+Ronycore@users.noreply.github.com>
2023-11-14 21:05:22 -0500

    Cambios

---
+++ Rony/book-recipe/book-recipe/pages/create-cooking-recipe/create-cooking-recipe.js
@@ -0,0 +1,11 @@
+const data ='nuevo dato';
+console.log('Hola mundo');
+
+data ='nuevo dato';
+console.log('Hola mundo');
+
+data ='otro dato';
+console.log('Hola mundo');
+
+data ='otro dato mas';
+console.log('Hola mundo');
