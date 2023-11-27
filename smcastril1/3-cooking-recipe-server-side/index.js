f38435174cbee0b82d47ce2134f877a16cb88052
SaraCastril1 <smcastril1@eafit.edu.co>
2023-11-26 19:37:19 -0500

    server

--- smcastril1/3-cooking-recipe-server-side/index.js
+++ smcastril1/3-cooking-recipe-server-side/index.js
@@ -8,6 +8,9 @@ const app = express();
 const { PORT } = process.env;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(router); //Midleware
 
 app.listen(PORT, () => {
--- smcastril1/3-cooking-recipe-server-side/router/recipe/put.js
+++ smcastril1/3-cooking-recipe-server-side/router/recipe/put.js
@@ -1,6 +1,21 @@
+require("dotenv").config();
+const fs = require("fs");
+const my_json = require("../../utils/json_manager");
+const { RECIPE_PATH } = process.env;
+
 //callback
 const add_recipe = (req, res) => {
-  res.send("Recipe created from put");
+  const my_body = req.body;
+  console.log(my_body);
+  const data = my_json.json_reader(RECIPE_PATH);
+  if (data.error) {
+    res.send(data.error);
+  } else {
+    console.log(my_body);
+    data.push(my_body);
+
+    res.status(201).send(my_body);
+  }
 };
 
 module.exports = { add_recipe };
